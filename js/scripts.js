01de3e4021f259f347226d47ee9ae0adadcb62ef
Keturah Howard and katy solovewicz <keturah@>
2020-01-21 10:57:53 -0800

    refactor the js to use forEach loops an decrease code repitition

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,32 @@
-$(document).ready(function() {
-  $("#formOne").submit(function(event) {
-    var noun1Input = $("input#noun1").val();
-    var pastTenseVerb1 = $("input#pastTenseVerb1").val();
-    var pastTenseVerb2 = $("input#pastTenseVerb2").val();
-    var typeOfPerson = $("input#typeOfPerson").val();
-    var pastTenseVerb3 = $("input#pastTenseVerb3").val();
-    var place = $("input#place").val();
-    var emotion = $("input#emotion").val();
+$(document).ready(function () {
+  $("#formOne").submit(function (event) {
 
-    $(".noun1").text(noun1Input);
-    $(".pastTenseVerb1").text(pastTenseVerb1);
-    $(".pastTenseVerb2").text(pastTenseVerb2);
-    $(".typeOfPerson").text(typeOfPerson);
-    $(".pastTenseVerb3").text(pastTenseVerb3);
-    $(".place").text(place);
-    $(".emotion").text(emotion);
+    var blanks = ["noun1", "pastTenseVerb1", "pastTenseVerb2", "typeOfPerson", "pastTenseVerb3", "place", "emotion"];
+
+    blanks.forEach(function (blank) {
+      var userInput = $("input#" + blank).val();
+      $("." + blank).text(userInput);
+    });
 
     $("#story").show();
 
     event.preventDefault();
   });
 });
+
+
+    // var noun1Input = $("input#noun1").val();
+    // var pastTenseVerb1 = $("input#pastTenseVerb1").val();
+    // var pastTenseVerb2 = $("input#pastTenseVerb2").val();
+    // var typeOfPerson = $("input#typeOfPerson").val();
+    // var pastTenseVerb3 = $("input#pastTenseVerb3").val();
+    // var place = $("input#place").val();
+    // var emotion = $("input#emotion").val();
+
+    // $(".noun1").text(noun1Input);
+    // $(".pastTenseVerb1").text(pastTenseVerb1);
+    // $(".pastTenseVerb2").text(pastTenseVerb2);
+    // $(".typeOfPerson").text(typeOfPerson);
+    // $(".pastTenseVerb3").text(pastTenseVerb3);
+    // $(".place").text(place);
+    // $(".emotion").text(emotion);
